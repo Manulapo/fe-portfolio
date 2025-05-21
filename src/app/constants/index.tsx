546ce8e6ea2e7009dc4b8a8f63278af200814fc9
Manulapo <manulaporta98@gmail.com>
2025-05-21 21:04:30 +0200

    cache seploy test

--- src/app/constants/index.tsx
+++ src/app/constants/index.tsx
@@ -37,7 +37,7 @@ export const sitePages = [
 
 export const userInfo = {
   name: 'Manuel La Porta',
-  title: 'Frontend Engineer & UX Designer',
+  title: 'Frontend Developer & UX Designer',
   location: 'Copenaghen, Capital region, Denmark',
   avatarImg,
   university: 'University of Pisa, Italy',
