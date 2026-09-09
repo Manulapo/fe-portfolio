b638279375cb0f909d23093070555b8dfb0c3976
Manulapo <manulaporta98@gmail.com>
2026-09-09 20:39:00 +0200

    change profile pic

--- src/app/constants/index.tsx
+++ src/app/constants/index.tsx
@@ -5,7 +5,7 @@ import notification from '@/assets/icons/notification.svg';
 import people from '@/assets/icons/people.svg';
 import publish from '@/assets/icons/publish.svg';
 import coverImg from '@/assets/images/coverImage2.jpeg';
-import avatarImg from '@/assets/images/profileImage.jpg';
+import avatarImg from '@/assets/images/profileImage2.jpg';
 import {
   AwardIcon,
   Briefcase,
